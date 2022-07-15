3bf390a10980c8557a3ad73d038de12ae265faff
biradarajay1904 <biradarajay04@gmail.com>
2022-07-15 17:05:48 +0530

    welcome to line comparision

---
+++ LineComparision.java
@@ -0,0 +1,15 @@
+package com.line.comparision;
+/**
+* ajay biradar
+
+
+*/
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision");
+		
+	}
+
+}
